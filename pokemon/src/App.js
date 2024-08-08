3c2c0cac71ffe0054f28532d04172a03fa0700a7
Otnielfreire <otnielaf@gmail.com>
2024-08-08 15:21:08 -0300

    Pokemon

---
+++ pokemon/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './styles/App.css'
+import PokemonList from './components/PokemonList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokémon App</h1>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
